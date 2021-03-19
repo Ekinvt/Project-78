4e9fd981d77d45b4e7f7b6d72f9c36964a318561
Ekinvt <71521059+Ekinvt@users.noreply.github.com>
2021-03-19 20:36:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ var images=[
     "https://thumbs.dreamstime.com/b/cartoon-happy-family-reading-book-illustration-139041623.jpg",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXNe3WicsSlb1j4s9S0Jt9xxHQwITviD2xDA&usqp=CAU",
     "https://www.pngitem.com/pimgs/m/157-1576261_free-png-chef-mothers-day-transparent-mom-clip.png",
-    "https://webstockreview.net/images/clap-clipart-boy-8.png",
+    "https://webstockreview.net/images/clap-clipart-boy-8.png"
     ];
-    var para=[" ","Dr Mahendra Pal","Dr Sunita Mohan","Mst Ekin Pal"
+    var para=["My Family","Dr Mahendra Pal","Dr Sunita Mohan","Mst Ekin Pal"
   
       ];
     var i= 0;
     function nextslide() {
-      if(i==9)
+      if(i==4)
         {
           i=0;
         }
       
      document.getElementById("Book").src=images[i];
-      i++;
-      document.getElementById("675").src=para[i];
+     
+      document.getElementById("675").innerHTML=para[i];
       i++;
     }
